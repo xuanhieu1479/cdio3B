1f60823e09326d404129e8613d646170bd79a4fb
Xuan Hieu <xuanhieu1479@gmail.com>
2019-05-08 05:31:31 +0700

    fix css

--- view/timkiem.php
+++ view/timkiem.php
@@ -8,7 +8,7 @@ include "../controller/timkiem.php";
     <meta charset="UTF-8">
     <title>Quản lý Homestay</title>
     <link rel="stylesheet" href="../css/TrangChuCSS/style.css">
-	<link rel="stylesheet" href="../css/TimkiemCSS/timkiem.css">
+	<link rel="stylesheet" href="../css/TimKiemCSS/timkiem.css">
 	<link rel="stylesheet" href="../css/bootstrap.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
